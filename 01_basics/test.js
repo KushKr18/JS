2d3c26a4b28d3b025a3de69c43113c47ae6913a2
KushKr18 <seriousforjee@gmail.com>
2025-04-24 10:41:30 +0000

    save progress

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,3 +1,15 @@
 console.log("hello")
 let number=5;
 console.log(number)
+
+//trying to see what output does this all codes gives 
+console.log(2+3)
+console.log(2-3)
+console.log(2*5)
+console.log(2**5)
+console.log("heelo whatsup")
+console.log("hello"+1)
+console.log("1"+5)
+console.log("1+"+5+4)
+console.log(4+5+"hello")
+console.log(4+5+"1")
